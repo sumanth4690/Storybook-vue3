2a23147af28318f4384d59d4258d5362f09241b5
sumanth <tsumanth@ariqt.com>
2024-11-17 12:28:46 +0530

    Added storybook project

---
+++ src/components/SButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button @click="emit('click')">{{ props.label }}Hello</button>
+  </div>
+</template>
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    label: string
+    primary?: boolean
+    size?: 'small' | 'medium' | 'large'
+  }>(),
+  { primary: false },
+)
+
+const emit = defineEmits<{
+  (e: 'click'): void
+}>()
+</script>
